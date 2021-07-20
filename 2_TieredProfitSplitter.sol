62e7f618b911506654aae16d2728a97fe627a547
William Chance <william@chance.tel>
2021-07-19 22:30:20 -0400

    Finished 2nd Tier Challenge

--- 2_TieredProfitSplitter.sol
+++ 2_TieredProfitSplitter.sol
@@ -22,13 +22,17 @@ contract TieredProfitSplitter {
         uint total;
         uint amount;
 
-        // @TODO: Calculate and transfer the distribution percentage
-        // Step 1: Set amount to equal `points` * the number of percentage points for this employee
-        // Step 2: Add the `amount` to `total` to keep a running total
-        // Step 3: Transfer the `amount` to the employee
+        amount = points * 75; // ceo get 75%
+        total += amount;
+        employee_one.transfer(amount);
 
-        // @TODO: Repeat the previous steps for `employee_two` and `employee_three`
-        // Your code here!
+        amount = points * 20; // cto get 20%
+        total += amount;
+        employee_two.transfer(amount);
+
+        amount = points * 5; // poor old bob gets 5%
+        total += amount;
+        employee_three.transfer(amount);
 
         employee_one.transfer(msg.value - total); // ceo gets the remaining wei
     }
